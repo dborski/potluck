1212b88fa09408973bbc3e4c7d15b6102702f59b
Derek Borski <derek.borski@gmail.com>
2020-03-23 11:37:00 -0600

    Adds first part of iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -10,9 +10,42 @@ class Potluck
     dishes << dish
   end
 
-  def get_all_from_category(dish)
+  def get_all_from_category(dish_type)
     selected_dishes = dishes.find_all do |dish|
-      dish.category.to_s.include?("appetizer")
+       dish.category == dish_type
     end
+    return selected_dishes
+  end
+
+  def menu
+    appetizers = get_all_from_category(:appetizer)
+    entres = get_all_from_category(:entre)
+    deserts = get_all_from_category(:dessert)
+
+    apps = appetizers.sort_by do |app|
+      app = app.name[0]
+    end
+
+    apps = apps.map do |app|
+      app = app.name
+    end
+
+    entres = entres.sort_by do |entre|
+      entre = entre.name[0]
+    end
+
+    entres = entres.map do |entre|
+      entre = entre.name
+    end
+
+    deserts = deserts.sort_by do |desert|
+      desert = desert.name[0]
+    end
+
+    deserts = deserts.map do |desert|
+      desert = desert.name
+    end
+
+    menu = {appetizers: apps, entres: entres, deserts: deserts}
   end
 end
